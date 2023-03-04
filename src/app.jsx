23087702c12d350e8d63e1f5611c58219f68814f
Marek Zelinka <mzelinka17@gmail.com>
2023-03-04 11:40:01 +0100

    create reducer using redux

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,24 @@
-export function App() {
-  return <div></div>;
+export function App(props) {
+  const { store } = props;
+
+  return (
+    <div>
+      <div>{store.getState()}</div>
+      <button
+        type="button"
+        onClick={() => store.dispatch({ type: "INCREMENT" })}
+      >
+        plus
+      </button>
+      <button
+        type="button"
+        onClick={() => store.dispatch({ type: "DECREMENT" })}
+      >
+        minus
+      </button>
+      <button type="button" onClick={() => store.dispatch({ type: "ZERO" })}>
+        zero
+      </button>
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createStore } from "redux";
 import { App } from "./app";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+function counterReducer(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENT": {
+      return state + 1;
+    }
+    case "DECREMENT": {
+      return state - 1;
+    }
+    case "ZERO": {
+      return 0;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+const store = createStore(counterReducer);
+
+const root = createRoot(document.getElementById("root"));
+
+function render() {
+  root.render(
+    <StrictMode>
+      <App store={store} />
+    </StrictMode>
+  );
+}
+
+render();
+store.subscribe(render);
